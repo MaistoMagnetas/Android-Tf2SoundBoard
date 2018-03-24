4bb765b9a77637e1816ff1f1802a89828aedd851
MaistoMagnetas <mindaugascel2@gmail.com>
2018-03-24 11:43:30 +0200

    Second commit. Updated background and grid view items

--- app/src/main/java/com/example/pc/tf2soundboard/CustomAdapter.java
+++ app/src/main/java/com/example/pc/tf2soundboard/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.example.pc.tf2soundboard;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,12 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by PC on 3/21/2018.
@@ -22,12 +26,17 @@ import java.util.List;
 public class CustomAdapter extends ArrayAdapter{
 
     ArrayList<Item> soundsList = new ArrayList<Item>();
+    int[] colorList = {R.color.gridItem3,R.color.gridItem4,R.color.gridItem5,
+            R.color.gridItem6,R.color.gridItem7};
+    RelativeLayout relativeLayout;
+
+    int gradientMapInteger = 0;
+    int colorPosition = 0;
 
     public CustomAdapter(Context context, int resource, ArrayList soundsList) {
         super(context, resource, soundsList);
         this.soundsList = soundsList;
     }
-
 
     @Override
     public int getCount() {
@@ -38,10 +47,34 @@ public class CustomAdapter extends ArrayAdapter{
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View v = convertView;
+
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         v = inflater.inflate(R.layout.grid_view_items, null);
         TextView textView = (TextView) v.findViewById(R.id.itemTextView);
         textView.setText(soundsList.get(position).getTitle());
+
+        //Randomlly colors grid view items
+//        relativeLayout = (RelativeLayout) v.findViewById(R.id.relativegriditem);
+//        Random rand = new Random();
+//        relativeLayout.setBackgroundResource(colorList[rand.nextInt(colorList.length)]);
+
+        //Sets gridview item colors gradient map method
+        relativeLayout = (RelativeLayout) v.findViewById(R.id.relativegriditem);
+        grandientMapBGonItems();
+
         return v;
+
+    }
+
+    private void grandientMapBGonItems(){ // sets bg for grid view items - like gradient map
+        gradientMapInteger++; //acts as constructor ID - kelinta karta kviecia metoda
+        int colorNum = colorList.length; // =5
+        int maxNum = 20; //TO-DO sound items array.
+        int eachColor = (maxNum/colorNum); // 20/5 == 4
+        if(gradientMapInteger>eachColor){
+            colorPosition++;
+        }
+        relativeLayout.setBackgroundResource(colorList[colorPosition]);
+
     }
 }
--- app/src/main/java/com/example/pc/tf2soundboard/SoundBoard.java
+++ app/src/main/java/com/example/pc/tf2soundboard/SoundBoard.java
@@ -13,9 +13,10 @@ import java.util.ArrayList;
 public class SoundBoard extends AppCompatActivity {
 
     RelativeLayout relativeLayout;
-
     GridView gridView;
     ArrayList soundList = new ArrayList<>();
+
+    String tf2class;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +25,63 @@ public class SoundBoard extends AppCompatActivity {
 
         //Get intent extra
         Intent intent = getIntent();
-        String tf2class = intent.getExtras().getString("chosenClass");
-
+        tf2class = intent.getExtras().getString("chosenClass");
         //Init grid view and items
-        gridView = (GridView) findViewById(R.id.gridView);
-        for(int i = 0; i<=5;i++){
-            soundList.add(new Item(tf2class));
-        }
+        //3x for testing aka Scrolling
+        makeGridItems();
+        makeGridItems();
+        makeGridItems();
+
         CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(),R.layout.grid_view_items,soundList);
         gridView.setAdapter(customAdapter);
 
         //Find layout to change its background accordingly to class selected
         relativeLayout = (RelativeLayout) findViewById(R.id.soundBoardBG);
+        changeBackgroundForClass();
+
 
         //Information about which class was selected in MainActivity
 
 
+    }
 
+    private void makeGridItems(){//Simple funciton CHange later?
         gridView = (GridView) findViewById(R.id.gridView);
-        for(int i = 0; i<=5;i++){
+        for(int i = 0; i<=5;i++){//TO-DO ilgi pakeisti i tiek kiek yra garsu
             soundList.add(new Item(tf2class));
         }
+    }
 
-
+    private void changeBackgroundForClass(){ // Works like a charm.  COuld be nicer though
+        switch (tf2class){
+            case "Scout":
+                relativeLayout.setBackgroundResource(R.drawable.scoutbgattempt);
+                break;
+            case "Soldier":
+                relativeLayout.setBackgroundResource(R.drawable.soldierbgattempt);
+                break;
+            case "Pyro":
+                relativeLayout.setBackgroundResource(R.drawable.pyrobgattempt);
+                break;
+            case "Demo":
+                relativeLayout.setBackgroundResource(R.drawable.demobgattempt);
+                break;
+            case  "Heavy":
+                relativeLayout.setBackgroundResource(R.drawable.heavybgattempt);
+                break;
+            case "Engi":
+                relativeLayout.setBackgroundResource(R.drawable.engibgattempt);
+                break;
+            case "Medic":
+                relativeLayout.setBackgroundResource(R.drawable.medicbgattempt);
+                break;
+            case "Sniper":
+                relativeLayout.setBackgroundResource(R.drawable.sniperbgattempt);
+                break;
+            case "Spy":
+                relativeLayout.setBackgroundResource(R.drawable.spybgattempt);
+                break;
+        }
 
     }
 }
